5a500948942dc2448dedb919dc7e73ee5abd8671
James Gray <james@james4k.com>
2017-07-17 14:59:01 -0500

    console: update VertexDecl

--- lime/graphics/console/VertexDecl.hx
+++ lime/graphics/console/VertexDecl.hx
@@ -3,10 +3,11 @@ package lime.graphics.console;
 
 @:enum abstract VertexDecl(Int) {
 
-	var Position = 0;				// xyz [3]f32
-	var PositionTexcoord = 5;		// xyz [3]f32, uv [2]f32
-	var PositionColor = 13;			// xyz [3]f32, rgba [4]u8
-	var PositionTexcoordColor = 4;	// xyz [3]f32, uv [2]f32, rgba [4]u8
+	var Position = 0;                   // xyz [3]f32
+	var PositionTexcoord = 6;           // xyz [3]f32, uv [2]f32
+	var PositionColor = 14;             // xyz [3]f32, rgba [4]u8
+	var PositionTexcoordColor = 4;      // xyz [3]f32, uv [2]f32, rgba [4]u8
+	var PositionTexcoordColorColor = 5;	// xyz [3]f32, uv [2]f32, rgba [4]u8, rgba [4]u8
 
 }
 
